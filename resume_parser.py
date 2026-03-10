2f9a1accd2b8a27a6878eb471bd61639f61cff5d
AsmitaTimalsena <020bscit011@sxc.edu.np>
2026-03-10 08:03:11 +0545

    first commit

---
+++ resume_parser.py
@@ -0,0 +1,27 @@
+'''
+author: 0x00-hello_world
+date: 2024-06-01
+feature: Hello World
+description: A simple program that prints "Hello, World!" to the console.
+implementation: how to use is described here: call api / payment/gateway to process and return response
+
+'''
+print("------------------THIS IS A RESUME PARSER------------------")
+name = input("Please enter your name: ")
+bio = input("Please enter your bio: ")
+email = input("Please enter your email: ")  
+phone = input("Please enter your phone number: ")
+address = input("Please enter your address: ")
+headline = input("Please enter your headline: ")
+photo_image_url = input("Please enter your photo image url: ")
+resume_url = input("Please enter your resume url: ")
+
+print("\n------------------RESUME PARSED------------------")
+print("Name:", name)
+print("Bio:", bio)
+print("Email:", email)
+print("Phone:", phone)
+print("Address:", address)
+print("Headline:", headline)
+print("Photo Image URL:", photo_image_url)
+print("Resume URL:", resume_url)
