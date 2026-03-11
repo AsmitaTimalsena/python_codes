a7b44b85f674945c7676a48941db04b0eee7a299
AsmitaTimalsena <020bscit011@sxc.edu.np>
2026-03-11 08:07:45 +0545

    added feature resume project counter

---
+++ resume_project_counter.py
@@ -0,0 +1,14 @@
+'''
+Author: Asmita
+Date: 2024-06-01
+Feature: Resume Project Counter
+Description: A simple program that counts the number of projects in a candidate's resume
+
+'''
+
+projects = int(input("How many projects are in your resume? "))
+
+for i in range(projects):
+   project = input("Enter project name: ")
+   print("Project added:", project)
+
