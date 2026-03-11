66a9def6cd9b8df3a355ad818ae51ae30e845553
AsmitaTimalsena <020bscit011@sxc.edu.np>
2026-03-11 08:06:46 +0545

    added feature experience analyzer

---
+++ resume_experience_analyzer.py
@@ -0,0 +1,16 @@
+'''
+Author: Asmita
+Date: 2024-06-01
+Feature: Resume Experience Analyzer
+Description: A simple program that analyzes the years of experience of a candidate and classifies them as
+'''
+
+experience = int(input("Please enter your years of experience: "))
+if experience < 1:
+    print("Fresher")
+elif experience <= 3:
+    print("Junior Developer")
+elif experience <= 6:
+    print("Mid-Level Developer")
+else:
+    print("Senior Developer")
