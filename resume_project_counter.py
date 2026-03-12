52e0ef53e44f6d8a34aacc3600da9166d962d718
AsmitaTimalsena <151120166+AsmitaTimalsena@users.noreply.github.com>
2026-03-12 07:25:37 +0545

    Refactor project counting into a function

--- resume_project_counter.py
+++ resume_project_counter.py
@@ -5,10 +5,11 @@ Feature: Resume Project Counter
 Description: A simple program that counts the number of projects in a candidate's resume
 
 '''
+def projectCounter():
+   projects = int(input("How many projects are in your resume? "))
+   
+   for i in range(projects):
+      project = input("Enter project name: ")
+      print("Project added:", project)
 
-projects = int(input("How many projects are in your resume? "))
-
-for i in range(projects):
-   project = input("Enter project name: ")
-   print("Project added:", project)
-
+projectCounter()
