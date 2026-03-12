81d5d0202f9117b3040b5240ca7cf4421e4b8102
AsmitaTimalsena <151120166+AsmitaTimalsena@users.noreply.github.com>
2026-03-12 07:26:11 +0545

    Refactor resume score calculation into a function

--- resume_score_calculator.py
+++ resume_score_calculator.py
@@ -5,15 +5,18 @@ Feature: Resume Score Calculator
 Description: A simple program that calculates the score of a candidate based on their skills
 
 '''
-score = 0
 
-python = input("Do you know Python? (yes/no): ")
-aws = input("Do you know AWS? (yes/no): ")
-
-if python == "yes":
-   score = score + 10
-
-if aws == "yes":
-   score = score + 10
-
-print("Resume Score:", score)
+def scoreCalculator():
+   score = 0
+   
+   python = input("Do you know Python? (yes/no): ")
+   aws = input("Do you know AWS? (yes/no): ")
+   
+   if python == "yes":
+      score = score + 10
+   
+   if aws == "yes":
+      score = score + 10
+   
+   print("Resume Score:", score)
+scoreCalculator()
