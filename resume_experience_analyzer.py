9187a14d84d8bd6a401b201ba409d532d6fab0bf
AsmitaTimalsena <151120166+AsmitaTimalsena@users.noreply.github.com>
2026-03-12 07:25:01 +0545

    Refactor experience classification into a function

--- resume_experience_analyzer.py
+++ resume_experience_analyzer.py
@@ -4,13 +4,15 @@ Date: 2024-06-01
 Feature: Resume Experience Analyzer
 Description: A simple program that analyzes the years of experience of a candidate and classifies them as
 '''
+def resume_experience_analyzer():
+    experience = int(input("Please enter your years of experience: "))
+    if experience < 1:
+        print("Fresher")
+    elif experience <= 3:
+        print("Junior Developer")
+    elif experience <= 6:
+        print("Mid-Level Developer")
+    else:
+        print("Senior Developer")
 
-experience = int(input("Please enter your years of experience: "))
-if experience < 1:
-    print("Fresher")
-elif experience <= 3:
-    print("Junior Developer")
-elif experience <= 6:
-    print("Mid-Level Developer")
-else:
-    print("Senior Developer")
+resume_experience_analyzer()
