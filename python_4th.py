9eac30263dc4c052790d6ac949e6e4adee17147e
AsmitaTimalsena <020bscit011@sxc.edu.np>
2026-03-17 09:37:07 +0545

    string manipulation

---
+++ python_4th.py
@@ -0,0 +1,12 @@
+#sequence of characters
+name = "Asmita"
+
+print(name[0])
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(len(name))
+
+name += " Timalsena"
+print(name)
